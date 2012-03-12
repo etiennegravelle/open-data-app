4e7167b7fcbfb6f10e9b7aa505c3246ec236da1f
etiennegravelle <etienne.gravelle@gmail.com>
2012-03-12 14:22:51 -0400

    working db file!

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
 // This is the best way to keep usernames and passwords out of public GitHub repos
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$dsn = getenv (stripslashes('DB_DSN'));
+$dsn = stripslashes(getenv('DB_DSN'));
 
 $db = new PDO($dsn, $user, $pass);
 
